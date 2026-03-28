34f67e79897faa8c57bea96b9110d9c62e7796e7
DominicDev <s0l0estudi4r@gmail.com>
2026-03-28 14:12:44 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RADIOHEAD';
+export let nameSongs = [
+	"CREED", /*1*/
+	"15 STEP", /*2*/
+	"PARANOID ANDROID", /*3*/
+	"STREET SPIRIT", /*4*/
+	"FAKE PLASTIC TREES", /*5*/
+	"NO SURPRISES", /*6*/
+	"EVERYTHING IN ITS RIGHT PLACE", /*7*/
+	"IDEOTEQUE", /*8*/
+	"KARMA POLICE", /*9*/
+	"HIGH AND DRY", /*10*/
+	"2 + 2 = 5", /*11*/
+	"EXIT MUSIC (FOR A FILM)", /*12*/
+	"HOW TO DISAPPEAR COMPLETELY", /*13*/
+	"PYRAMID SONG", /*14*/
+	"JUST", /*15*/
+	"LOTUS FLOWER", /*16*/
+	"BODYSNATCHERS", /*17*/
+	"ALL I NEED", /*18*/
+	"RECKONER", /*19*/
+	"TALK SHOW HOST", /*20*/
+]
